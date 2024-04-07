34b1631722d469219cf04bdaa9b86e5a2e569524
Vladislav Suchkov <v.suchkov@yadro.com>
2024-04-07 16:55:57 +0300

    Tables

---
+++ src/dataTypes/include/dataTypes/common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace nm {
+namespace dtypes {
+enum struct ElemType {
+    uint8 = 0,
+    uint32 = 1,
+    uint64,
+    int8 ,
+    int32,
+    int64,
+    fp32,
+    fp64
+};
+} // namespace dtypes
+} // namespace nm
