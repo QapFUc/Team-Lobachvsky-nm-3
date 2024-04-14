57d8fe56cb2700eadf065c7ba5c6319b0fd6675c
QapFUc <qapfuc@gmail.com>
2024-04-14 14:27:43 +0300

    TMA test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <cstddef>
 #include <iostream>
+#include "dataTypes/include/dataTypes/common.hpp"
 #include "nm/utils/profiler.hpp"
 #include "nm/utils/logger.hpp"
+#include "core/TMA.hpp"
 
 double testKernel(const double& a, nm::Timer& t, nm::Profiler& prof) {
 	nm::Timer f{ nm::Timer::scopeTimer(&t)};
@@ -19,6 +21,27 @@ double testKernel(const double& a, nm::Timer& t, nm::Profiler& prof) {
 
 	return res;
 }
+void testMyFunc(const double *A, const double *F, double *X, const int n,nm::Profiler& prof) {
+	nm::Profiler p{ prof.nest(nm::GET_CURRENT_SCOPE())};
+	nm::Status func = ProcessTMA(A,F,X,n,p);
+}
+
+void testRefFunc(double coeffs[][3],double *rhs, double *x, const size_t& n,nm::Profiler& prof) {
+	nm::Profiler p{ prof.nest(nm::GET_CURRENT_SCOPE())};
+	double temp;
+    for (size_t i = 1; i < n; i++) { 
+        temp = coeffs[i][0] / coeffs[i - 1][1];
+        coeffs[i][1] = coeffs[i][1] - temp * coeffs[i - 1][2];
+        rhs[i] = rhs[i] - temp * rhs[i - 1];
+    }
+
+    x[n - 1] = rhs[n - 1] / coeffs[n - 1][1];
+
+    for (long long int i = n - 2; i >= 0; i--) {
+        x[i] = (rhs[i] - coeffs[i][2] * x[i + 1]) / coeffs[i][1];
+    }
+}
+
 
 int main(int argc, char* argv[]) {
 	nm::Profiler prof;
@@ -26,6 +49,15 @@ int main(int argc, char* argv[]) {
 
 	std::cout << "got prof" << '\n';
 	double res = testKernel(1.05l, t, prof);
+	double coeffs[5][3] = {{0.0,2.0,3.0},{1.0,2.0,3.0},{1.0,2.0,3.0},{1.0,2.0,3.0},{1.0,2.0,0.0}}; 
+	double X_1[5];
+	double X_2[5];
+	double RHS[] = {5,6,7,8,9};
+	double result_A[] = {0.0,2.0,3.0,1.0,2.0,3.0,1.0,2.0,3.0,1.0,2.0,3.0,1.0,2.0,0.0};
+	std::cout << "MyFunc start" << '\n';
+	testMyFunc(result_A,RHS,X_1,5,prof);
+	std::cout << "RefFunc start" << '\n';
+	testRefFunc(coeffs,RHS,X_2,5,prof);
 	std::cout << "kernel passed" << '\n';
 	auto profdata = prof.getAllData();
 	std::cout << "Profiling data" << '\n';
@@ -35,5 +67,8 @@ int main(int argc, char* argv[]) {
 
 	std::cout << t.getClocks() << '\n';
 	std::cout << t.getDuration() << '\n';
+	for(int i = 0;i<5;++i) {
+		std::cout << "my = "<<X_1[i]<<" ref ="<<X_2[i]<<",";
+	}
 	return 0;
 }
