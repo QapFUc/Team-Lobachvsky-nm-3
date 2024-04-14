928be8357e3d29d784d97b65ccdfd873b4b47208
QapFUc <qapfuc@gmail.com>
2024-04-14 15:06:21 +0300

    TMA test done

--- src/core/src/TMA.cpp
+++ src/core/src/TMA.cpp
@@ -2,35 +2,18 @@
 #include "dataTypes/common.hpp"
 #include "nm/utils/profiler.hpp"
 
-nm::Status ProcessTMA(const double *A, const double *F, double *X, const int &n, nm::Profiler& prof) {
-
-    // TODO: TMA Algorithm
-    double result_A[n];
-    double result_F[n%3];
+nm::Status ProcessTMA(double *A, double *F, double *X, const int &n, nm::Profiler& prof) {
     double mult;
-    for(int i = 0; i < n; i++) {
-        result_A[i] = A[i];
-        result_F[i] = F[i];
+    for (size_t i = 1; i < n; ++i) {
+        mult = A[i * 3] / A[(i - 1) * 3 + 1];
+        A[i * 3 + 1] = A[i * 3 + 1] - mult * A[(i - 1) * 3 + 2];
+        F[i] = F[i] - mult * F[i - 1];
     }
-     nm::Profiler localProf = prof.nest(nm::GET_CURRENT_SCOPE());
-    for (size_t i = 0; i < n-4; i= i+3) {
-        if (result_A[i+1] != 0) {
-            mult = result_A[i+3]/result_A[i+1];
-            result_A[i+3] = result_A[i+3] - mult * result_A[i+1];
-            result_A[i+4] = result_A[i+4] - mult * result_A[i+2];
-            result_F[i%3+1] = result_F[i%3+1] - mult* result_F[i%3];
-        }
-    }
-    for (size_t i = n-1; i > 4; i= i-3) {
-        if (result_A[i-1] != 0) {
-            mult = result_A[i-3]/result_A[i-1];
-            result_A[i-3] = result_A[i-3] - mult * result_A[i-1];
-            result_A[i-4] = result_A[i-4] - mult * result_A[i-2];
-            result_F[i%3-1] = result_F[i%3-1] - mult* result_F[i%3];
-        }
-    }
-    for(size_t i = 0; i< n%3; ++i) {
-        X[i] = result_F[i] / result_A[i*3+1];
+
+    X[n - 1] = F[n - 1] / A[(n - 1) * 3 + 1];
+
+    for (long long i = n - 2; i >= 0; i--) {
+        X[i] = (F[i] - A[i * 3 + 2] * X[i + 1]) / A[i * 3 + 1];
     }
 
     return nm::Status::Success;
