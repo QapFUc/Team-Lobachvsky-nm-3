1662a8f83791e146150d07b45a7179dbd5622b13
QapFUc <qapfuc@gmail.com>
2024-05-18 11:09:46 +0300

    0.4V

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,18 +1,15 @@
 #include "widget.h"
 
 #include <QApplication>
-#include <QProcess>
-#include <QStyleFactory>
-
-
 
 sig_atomic_t exitFlag = 0;
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    Widget w;
-
+    Widget w; 
     w.show();
     return app.exec();
 }
